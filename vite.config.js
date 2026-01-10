cbe7f5782a46e7964163a259ce1f46154859cc47
jair-chaguay <jchaguay@espol.edu.ec>
2026-01-09 21:00:28 -0500

    s

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ParkfastDashb/"
+  base: "https://jair-chaguay.github.io/ParkfastDashb"
 })
