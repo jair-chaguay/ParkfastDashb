d2d41e104e733168409e4ea7fb3470a22744412f
jair-chaguay <jchaguay@espol.edu.ec>
2026-01-09 21:28:34 -0500

    s

--- src/hooks/useParkingData.js
+++ src/hooks/useParkingData.js
@@ -4,23 +4,20 @@ export default function useParkingData() {
   const [dashboard, setDashboard] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch(`${import.meta.env.BASE_URL}data/dashboardData.json`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(`HTTP ${res.status}`);
-        }
-        return res.json();
-      })
-      .then(json => {
-        setDashboard(json);
-        setLoading(false);
-      })
-      .catch(err => {
-        console.error("Error cargando dashboardData:", err);
-        setLoading(false);
-      });
-  }, []);
+
+    useEffect(() => {
+  fetch(`${import.meta.env.BASE_URL}data/dashboardData.json`)
+    .then((res) => {
+      if (!res.ok) throw new Error(`HTTP ${res.status}`);
+      return res.json();
+    })
+    .then((data) => {
+      setDashboard(data);
+      setLoading(false);
+    })
+    .catch(err => console.error("Error cargando dashboardData:", err));
+}, []);
+
 
   return { dashboard, loading };
 }
